3b3e9babd8c56c4221d4414f399c5a59afeaaa51
AayuseUwuu <aashilyshrestha@clubmember.org>
2024-12-20 12:53:41 +0900

    Update proxima.js

--- scripts/cmds/proxima.js
+++ scripts/cmds/proxima.js
@@ -36,7 +36,7 @@ module.exports = {
       const res = await axios.get(`https://sandipbaruwal.onrender.com/gemini?prompt=${encodedPrompt}`);
       const result = res.data.answer;
 
-      api.setMessageReaction("âœ…", event.messageID, () => { }, true);
+      api.setMessageReaction("🕕", event.messageID, () => { }, true);
       message.reply({
         body: `${name}, ${result}`,
         mentions: ment,
@@ -60,11 +60,11 @@ module.exports = {
       const ment = [{ id: id, tag: name }];
       const prompt = args.join(" ");
       const encodedPrompt = encodeURIComponent(prompt);
-      api.setMessageReaction("â³", event.messageID, () => { }, true);
+      api.setMessageReaction("🕕", event.messageID, () => { }, true);
       const res = await axios.get(`https://sandipbaruwal.onrender.com/gemini?prompt=${encodedPrompt}`);
       const result = res.data.answer;
 
-      api.setMessageReaction("âœ…", event.messageID, () => { }, true);
+      api.setMessageReaction("✅", event.messageID, () => { }, true);
       message.reply({
         body: `${name}, ${result}`,
         mentions: ment,
